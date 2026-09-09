5853056ca30b326948bb942922ef46a82ea678f9
RatiloeLebogang <mratwalebogang@gmail.com>
2026-09-09 12:06:54 +0000

    Add an image for varsity path project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,21 +1,5 @@
 const projects = [
-  {
-    title: "RoadReady Traffic Services",
-    desc: "Full-stack platform unifying licence bookings, vehicle registration, fines, and applications for citizens and admins.",
-    tags: ["React", "TypeScript", "Supabase", "PostgreSQL"],
-    role: "Frontend Developer",
-    image: "/images/road-ready-service.jpeg",
-    github: "https://github.com/RatiloeMbonani/RoadReady-Traffic-Services.git",
-  },
-  {
-    title: "RoadReady Insights",
-    desc: "Second phase of RoadReady adding a citizen feedback system with sentiment analysis and an admin analytics dashboard.",
-    tags: ["React", "TypeScript", "Tailwind CSS","Vader", "Hugging Face","Supabase" ],
-    role: "Backend Developer",
-    image: "/images/road-ready-insight.jpeg",
-    github: "https://github.com/RatiloeMbonani/roadready-feedback-insights.git",
-  },
-  {
+   {
     title: "InkWave AI",
     desc: "Full-stack, multi-modal AI content platform turning one idea into blogs, social posts, emails, images, video, code, and audio.",
     tags: [ "React", "TypeScript", "Tailwind CSS", "Supabase",],
@@ -33,6 +17,23 @@ const projects = [
         github: "https://github.com/RatiloeMbonani/Varsity_Path.git"
 },
 
+  {
+    title: "RoadReady Traffic Services",
+    desc: "Full-stack platform unifying licence bookings, vehicle registration, fines, and applications for citizens and admins.",
+    tags: ["React", "TypeScript", "Supabase", "PostgreSQL"],
+    role: "Frontend Developer",
+    image: "/images/road-ready-service.jpeg",
+    github: "https://github.com/RatiloeMbonani/RoadReady-Traffic-Services.git",
+  },
+  {
+    title: "RoadReady Insights",
+    desc: "Second phase of RoadReady adding a citizen feedback system with sentiment analysis and an admin analytics dashboard.",
+    tags: ["React", "TypeScript", "Tailwind CSS","Vader", "Hugging Face","Supabase" ],
+    role: "Backend Developer",
+    image: "/images/road-ready-insight.jpeg",
+    github: "https://github.com/RatiloeMbonani/roadready-feedback-insights.git",
+  },
+ 
 ];
 
 export default projects;
